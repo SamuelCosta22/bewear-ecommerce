e8edbbae3e55f877131c21c8585712dc9d659072
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-08-07 14:54:21 -0300

    feat: add quantity selector component to product variant page

--- src/app/product-variant/[slug]/page.tsx
+++ src/app/product-variant/[slug]/page.tsx
@@ -10,6 +10,7 @@ import { db } from "@/db";
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
 
+import QuantitySelector from "./components/quantity-selector";
 import VariantSelector from "./components/variant-selector";
 
 interface ProductVariantPageProps {
@@ -73,7 +74,9 @@ const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
           </h3>
         </div>
 
-        <div className="px-5"></div>
+        <div className="px-5">
+          <QuantitySelector />
+        </div>
 
         <div className="flex flex-col space-y-2 px-5">
           <Button size="lg" variant="outline" className="gap-2">
